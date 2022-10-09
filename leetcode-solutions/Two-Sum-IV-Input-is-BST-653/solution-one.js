f7741a1986b19ccb835034aa760add4d1ae9f9a6
kuzzzzz <atumahdaniel@gmail.com>
2022-10-09 22:44:13 +0100

    leetcode daily challenge solution qes 653

---
+++ leetcode-solutions/Two-Sum-IV-Input-is-BST-653/solution-one.js
@@ -0,0 +1,23 @@
+debugger;
+const insertLevelOrder = require("../util/createBinaryTree");
+
+var findTarget = function (root, k) {
+  let set = new Set();
+  return helper(root, set, k);
+};
+const helper = (root, set, k) => {
+  if (!root) return false;
+  if (set.has(k - root.val)) return true;
+  set.add(root.val);
+  return helper(root.left, set, k) || helper(root.right, set, k);
+};
+
+//Test cases
+let arr = [5, 3, 6, 2, 4, null, 7];
+let binTree = insertLevelOrder(arr, 0);
+
+let arr1 = [5, 3, 6, 2, 4, null, 7];
+let binTree1 = insertLevelOrder(arr1, 0);
+
+findTarget(binTree, 9);
+findTarget(binTree1, 28);
