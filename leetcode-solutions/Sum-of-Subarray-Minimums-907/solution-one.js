49dae39e89ea92837a3a0b520042e511cbcd2370
kuzzzzz <atumahdaniel@gmail.com>
2022-11-25 19:48:43 +0100

    leetcode daily challenge solution qes.907

---
+++ leetcode-solutions/Sum-of-Subarray-Minimums-907/solution-one.js
@@ -0,0 +1,26 @@
+debugger;
+var sumSubarrayMins = function (arr) {
+  M = 10 ** 9 + 7;
+  stack = [-1];
+  res = 0;
+  arr.push(0);
+
+  for (let i2 = 0; i2 < arr.length; i2++) {
+    while (arr[i2] < arr[stack[stack.length - 1]]) {
+      i = stack.pop();
+      i1 = stack[stack.length - 1];
+      Left = i - i1;
+      Right = i2 - i;
+      res += Left * Right * arr[i];
+    }
+    stack.push(i2);
+  }
+
+  return res % M;
+};
+
+let test1 = sumSubarrayMins([3, 1, 2, 4]);
+
+let test2 = sumSubarrayMins([11, 81, 94, 43, 3]);
+
+console.log(test1, test2);
