3d1ecf1ed8d6db50d0fc7d67e57e0e55abea8c59
kuzzzzz <atumahdaniel@gmail.com>
2023-01-02 23:05:46 +0100

    leetcode daily challenge solution qes. 520

---
+++ leetcode-solutions/Detect-Capital-520/solution-one.js
@@ -0,0 +1,18 @@
+var detectCapitalUse = function (word) {
+  if (word.toUpperCase() === word) {
+    return true;
+  }
+
+  if (word.toLowerCase() === word) {
+    return true;
+  }
+
+  if (
+    word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase() ===
+    word
+  ) {
+    return true;
+  }
+
+  return false;
+};
