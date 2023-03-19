27f1e02911efb7832a494a31d4d8fc35eb9b183b
kuzzzzz <atumahdaniel@gmail.com>
2023-03-19 08:45:27 +0100

    leetcode daily challenge solution qes. 211

---
+++ leetcode-solutions/Design-Add-and-Search-Words-Data-Structure-211/solution-one.js
@@ -0,0 +1,41 @@
+var WordDictionary = function () {
+  this.trie = {};
+};
+
+/**
+ * Adds a word into the data structure.
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function (word) {
+  let root = this.trie;
+  for (let i = 0; i < word.length; i++) {
+    if (root[word[i]] == null) root[word[i]] = {};
+    root = root[word[i]];
+  }
+  root.isEnd = true;
+};
+
+/**
+ * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function (word) {
+  return this.dfs(word, 0, this.trie);
+};
+
+WordDictionary.prototype.dfs = function (word, index, node) {
+  if (index == word.length) return node.isEnd == true;
+
+  if (word[index] == ".") {
+    for (let key in node) {
+      if (this.dfs(word, index + 1, node[key])) return true;
+    }
+  } else {
+    if (node[word[index]] != null) {
+      return this.dfs(word, index + 1, node[word[index]]);
+    }
+  }
+  return false;
+};
