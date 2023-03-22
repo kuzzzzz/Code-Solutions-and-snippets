8739da90dcd8be66c2f783b0f1880b9e8ee2fc1d
kuzzzzz <atumahdaniel@gmail.com>
2023-03-22 18:12:59 +0100

    leetcode daily challenge solution qes. 2492

---
+++ leetcode-solutions/Minimum-Score-of-a-Path-Between-Two-Cities-2492/solution-one.js
@@ -0,0 +1,38 @@
+var minScore = function (n, roads) {
+  let hash = {};
+  let ans = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < roads.length; i++) {
+    let x = roads[i][0];
+    let y = roads[i][1];
+    let d = roads[i][2];
+
+    if (hash[x]) {
+      hash[x].push([y, d]);
+    } else {
+      hash[x] = [];
+      hash[x].push([y, d]);
+    }
+
+    if (hash[y]) {
+      hash[y].push([x, d]);
+    } else {
+      hash[y] = [];
+      hash[y].push([x, d]);
+    }
+  }
+
+  let visited = {};
+  function solve(x, hash) {
+    for (let y of hash[x]) {
+      ans = Math.min(ans, y[1]);
+      if (!visited[y[0]]) {
+        visited[y[0]] = true;
+        solve(y[0], hash);
+      }
+    }
+  }
+
+  solve(1, hash);
+  return ans;
+};
