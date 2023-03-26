fefb3df075552cbb954d5d7516e2bf5c32505fc3
kuzzzzz <atumahdaniel@gmail.com>
2023-03-26 23:09:39 +0100

    leetcode daily challenge solution qes. 2360

---
+++ leetcode-solutions/Longest-Cycle-in-a-Graph-2360/soluton-one.js
@@ -0,0 +1,26 @@
+function longestCycle(edges) {
+  const n = edges.length;
+  const visited = Array(n).fill(false);
+  let maxCyclePath = -1;
+  for (let i = 0; i < n; i++) {
+    if (visited[i]) {
+      continue;
+    }
+    const paths = new Map();
+    let distance = 0;
+    for (let j = i; j !== -1; j = edges[j]) {
+      if (paths.has(j)) {
+        maxCyclePath = Math.max(maxCyclePath, distance - paths.get(j));
+        break;
+      }
+      if (visited[j]) {
+        break;
+      }
+      visited[j] = true;
+      paths.set(j, distance);
+      distance++;
+    }
+  }
+
+  return maxCyclePath;
+}
