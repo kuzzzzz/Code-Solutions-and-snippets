0e482b56695c8931884314c2ff0b4c327da09f4b
kuzzzzz <atumahdaniel@gmail.com>
2023-05-04 10:10:35 +0100

    leetcode daily challenge solution qes. 649

---
+++ leetcode-solutions/Dota2-Senate-649/solution-one.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} senate
+ * @return {string}
+ */
+let predictPartyVictory = function (senate) {
+  let sen = Array.from(senate, String);
+  let count = 0;
+  let cur = sen[0];
+  while (true) {
+    for (let i = 0; i < sen.length; i++) {
+      if (sen[i] == "0") {
+        continue;
+      }
+      if (cur == sen[i]) {
+        count++;
+      } else {
+        count--;
+
+        if (count < 0) {
+          count = 1;
+          cur = sen[i];
+        } else {
+          sen[i] = "0";
+        }
+      }
+    }
+    let r = 0,
+      d = 0;
+    for (let i = 0; i < sen.length; i++) {
+      if (sen[i] == "R") {
+        r++;
+      }
+      if (sen[i] == "D") {
+        d++;
+      }
+    }
+    if (r == 0) {
+      return "Dire";
+    }
+    if (d == 0) {
+      return "Radiant";
+    }
+  }
+};
